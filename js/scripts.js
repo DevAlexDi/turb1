878924a0109bd6b32a0cf18fe5f0d5d8a3cdf37b
unknown <keitmark@mail.ru>
2018-08-06 15:49:40 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    
+//    
+//    $('.tel-init').inputmask({
+//        "mask": "+7 (999) 999-99-99"
+//        , "placeholder": "_"
+//        , showMaskOnHover: false
+//        , showMaskOnFocus: true
+//    });
+//    
+    
+});
