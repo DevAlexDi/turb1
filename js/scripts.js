2c6adb982133a452bffc7019cd191129d090e34f
unknown <keitmark@mail.ru>
2018-08-09 13:38:22 +0300

    save

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,67 @@
 $(document).ready(function () {
-    
+    $('.main-slider-init').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+        arrows: false
+    });
 //    
 //    $('.tel-init').inputmask({
 //        "mask": "+7 (999) 999-99-99"
 //        , "placeholder": "_"
 //        , showMaskOnHover: false
 //        , showMaskOnFocus: true
-//    });
-//    
+//    }); 
+//   
+    
+    $('.sec-slider-category .tab-pane').each(function() {
+        if ($(this).find('.slider-ekz .one-slide').length > 3) {
+            $(this).find('.slider-ekz').slick({
+                dots: false,
+                infinite: true,
+                speed: 300,
+                slidesToShow: 3,
+                centerMode: true,
+                centerPadding: '0px',
+                focusOnSelect: true,
+                responsive: [
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            centerMode: false
+                        }
+                    }
+                ]
+            });
+        }
+        else {
+            $(this).find('.slider-ekz').slick({
+                dots: false,
+                infinite: true,
+                speed: 300,
+                slidesToShow: 3,
+                focusOnSelect: true,
+                responsive: [
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            centerMode: false
+                        }
+                    }
+                ]
+            });
+        }
+    });
+    
+    
+    
+    
+    
     
 });
